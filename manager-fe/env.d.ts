8f75981b2093884bbbfcdaedbc493a250ba8e4f8
zengfancheng001 <863513651@qq.com>
2023-12-24 16:25:49 +0800

    初始化

--- manager-fe/env.d.ts
+++ manager-fe/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// vue文件声明
+declare module "*.vue" {
+  import { DefineComponent } from 'vue'
+  const component:DefineComponent
+  export default component
+}
